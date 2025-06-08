67fdd9dc1ab8eb124b6c64a81f0f2dc43aee798b
Стас Басов <stas.basov@whatever.com>
2025-06-08 17:31:37 +0300

    Fixed after Review 2

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@
 const cardTemplate = document.querySelector('#card-template');
 const placesList = document.querySelector('.places__list');
 
-function createCard({name, link}) {
+function createCard({name, link}, deleteCard) {
   const newCardTemplate = cardTemplate.content.cloneNode(true);
   const cardDeleteButton = newCardTemplate.querySelector('.card__delete-button');
   const cardElment = newCardTemplate.querySelector('.card'); 
@@ -15,19 +15,20 @@ function createCard({name, link}) {
   newCardTemplate.querySelector('.card__image').src = link;
   newCardTemplate.querySelector('.card__title').textContent = name;
   newCardTemplate.querySelector('.card__image').alt = name;
- 
+
   cardDeleteButton.addEventListener('click', () => {
-    cardElment.remove();
+    deleteCard(name, cardElment);
   });
 
   return newCardTemplate;
 };
 
-function onDeleteClick(cardNameItem) {
-  console.log(`Удаление карточки ${cardNameItem}.`);
-};
+function deleteCard(name, cardElment) {
+  console.log(`Удаление карточки "${name}".`);
+  cardElment.remove();
+}
 
 initialCards.forEach(cardItems => {
-  const cardItem = createCard(cardItems, onDeleteClick);
+  const cardItem = createCard(cardItems, deleteCard);
   placesList.appendChild(cardItem);
 });
